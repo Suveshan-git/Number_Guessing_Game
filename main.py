c269e3d8cf020d0d735748a68b95f583be406572
Suveshan Moodley <suveshan.moodley@gmail.com>
2020-11-10 14:35:04 +0200

    Completed and tested code for game

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 # NUMBER GUESSING GAME
+import random
+
+# Setup the range
+range_set = int(input("Enter a number you want the range to be set to: "))
+print(f"The range is set to (1-{range_set})")
+random_number = random.randint(1, range_set)
+# print(random_number)
+
+# Ask the user to guess
+tries = 1
+guess = int(input("Enter your guess: "))
+
+# Check if the guess hit, was too low or was too high
+while guess != random_number:
+    if guess < random_number:
+        print("Too low")
+        guess = int(input("Enter your guess: "))
+        tries += 1
+    elif guess > random_number:
+        print("Too high")
+        guess = int(input("Enter your guess: "))
+        tries += 1
+
+tries +=1
+print(f"HIT! Congratulations! You got it in {tries} tries.")
+
