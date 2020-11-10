0da15aad5a37b7aa990ce269a0e68e5c43e06f75
Suveshan Moodley <suveshan.moodley@gmail.com>
2020-11-10 13:53:14 +0200

    added code to welcome and ask user their name

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello")
+import random
+
+# Welcome the user to the game and ask for their name
+print("Welcome to the Number Guessing Game!")
+player_name = input("What is your name? ")
+
+# Greet the user and ask for their guess
+guess = input(f"Hi {player_name}! A random number from 1-10 has been generated. Let's see if you can guess what it is.")
