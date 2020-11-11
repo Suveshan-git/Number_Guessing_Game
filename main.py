aa1db21fc7d23fa43b1013c2cc216e73f72b94c9
Suveshan Moodley <suveshan.moodley@gmail.com>
2020-11-11 13:21:16 +0200

    Completed and tested code for game

--- main.py
+++ main.py
@@ -5,22 +5,25 @@ import random
 range_set = int(input("Enter a number you want the range to be set to: "))
 print(f"The range is set to (1-{range_set})")
 random_number = random.randint(1, range_set)
-# print(random_number)
+print(random_number) # - Testing purposes
 
 # Ask the user to guess
 tries = 1
-guess = int(input("Enter your guess: "))
+guess = int(input("Guess the random number: "))
 
 # Check if the guess hit, was too low or was too high
 while guess != random_number:
-    if guess < random_number:
+    if 0 < guess < random_number:
         print("Too low")
-        guess = int(input("Enter your guess: "))
+        guess = int(input("Guess the random number: "))
         tries += 1
-    elif guess > random_number:
+    elif random_number < guess <= int(range_set):
         print("Too high")
-        guess = int(input("Enter your guess: "))
+        guess = int(input("Guess the random number: "))
         tries += 1
+    else:
+        print("ERROR: Invalid input")
+        guess = int(input("Guess the random number: "))
 
 tries +=1
 print(f"HIT! Congratulations! You got it in {tries} tries.")
